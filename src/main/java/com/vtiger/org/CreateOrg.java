03dcf415781c31b2e89d4685b4244ab3d8512ff3
shashi <shashikr9078@gmail.com>
2022-03-24 14:07:02 +0530

    manager-first commit

---
+++ src/main/java/com/vtiger/org/CreateOrg.java
@@ -0,0 +1,14 @@
+package com.vtiger.org;
+
+import org.testng.annotations.Test;
+
+public class CreateOrg {
+
+	@Test
+	public void createOrg() {
+		
+		System.out.println("Manager-Created Orgnation");
+
+	}
+
+}
