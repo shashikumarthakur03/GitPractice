bc6ab0aae5210056435ebbff779fb5497367bdd5
shashi <shashikr9078@gmail.com>
2022-03-25 09:00:52 +0530

    manager commit

---
+++ src/test/java/com/nt/lead/CreateLead.java
@@ -0,0 +1,13 @@
+package com.nt.lead;
+
+import org.testng.annotations.Test;
+
+public class CreateLead {
+	
+	@Test
+	public void createLead() {
+		System.out.println("Lead Created by Manager");
+		
+	}
+
+}
